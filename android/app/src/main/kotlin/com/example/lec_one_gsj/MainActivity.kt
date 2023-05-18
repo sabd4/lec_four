45d576e8c3d756bd9895ba09d0264e47da796a1b
Sondos Abo Shaweesh <sabd41078@gmail.com>
2023-05-18 16:10:16 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/lec_one_gsj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lec_one_gsj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        home: Scaffold(
+      body: Center(
+        child: Text(
+          "Sondos",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    ));
+  }
+}
